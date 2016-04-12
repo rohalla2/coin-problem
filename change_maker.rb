3a67e4814f9fc575c7659e2feb289a8880a72b71
Jeff Rohalla <jeffrohalla@gmail.com>
2016-04-12 14:04:42 -0500

    initial commit

---
+++ change_maker.rb
@@ -0,0 +1,14 @@
+class ChangeMaker
+  # Returns an array of the least amount of coins required to get to 'amount'
+  # **Assumption** Change can always be made - BONUS POINTS - raise an error if
+  # change can not be made
+  # Params:
+  # +amount+:: The amount to make change for
+  # +denominations+:: An array containing the denominations that can be used.
+  #                   Defaults to standard US coin denominations
+  def self.make_change(amount, denominations=[1,5,10,25])
+    raise "Not Implemented"
+  end
+end
+
+class ChangeError < StandardError; end
---
+++ change_maker_spec.rb
@@ -0,0 +1,41 @@
+require_relative 'change_maker'
+
+describe ChangeMaker do
+  describe ".make_change" do
+    it "creates change for 1 cent" do
+      result = ChangeMaker.make_change(1)
+      
+      expect(result).to match_array([1])
+    end
+
+    it "creates change for 6 cents" do
+      result = ChangeMaker.make_change(6)
+      
+      expect(result).to match_array([1, 5])
+    end
+
+    it "creates change for 37 cents" do
+      result = ChangeMaker.make_change(37)
+      
+      expect(result).to match_array([25, 10, 1, 1])
+    end
+
+    it "creates change for 5 cents given different denominations" do
+      ChangeMaker.make_change(5, [1, 3])
+      
+      expect(result).to match_array([3, 1, 1])
+    end
+
+    it "creates change for 17 cents given different denominations" do
+      ChangeMaker.make_change(17, [1, 8, 10])
+      
+      expect(result).to match_array([8, 8, 1])
+    end
+
+    it "raises an error if there is no way to make change" do
+      expect {
+        ChangeMaker.make_change(17, [5, 8, 10])
+      }.to raise_error(ChangeError)
+    end
+  end
+end
