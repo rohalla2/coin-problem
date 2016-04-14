de2577051282333c25fefcab47c8ac19a2b66ece
Jeff Rohalla <jeffrohalla@gmail.com>
2016-04-13 21:34:42 -0500

    Add DP solution

--- change_maker.rb
+++ change_maker.rb
@@ -7,17 +7,39 @@ class ChangeMaker
   # +denominations+:: An array containing the denominations that can be used.
   #                   Defaults to standard US coin denominations
   def self.make_change(amount, denominations=[1,5,10,25])
-    solution = []
-    denominations.reverse.each do |coin|
-      (amount/coin).floor.times do
-        solution << coin
-        amount -= coin
-      end
+    num_coins_needed = []
+    solutions_array = []
+
+    # initialize arrays
+    (0..amount).each do |val|
+      num_coins_needed[val] = nil
+      solutions_array[val] = []
     end
-    if amount != 0
+
+    # establish base case
+    num_coins_needed[0] = 0
+    solutions_array[0] = []
+
+    (1..amount).each do |val|
+      current_coins_needed = Float::INFINITY
+      current_coin_solution = []
+
+      denominations.each do |coin|
+        if coin <= val
+          if (num_coins_needed[val - coin] + 1) < current_coins_needed
+            current_coins_needed = num_coins_needed[val - coin] + 1
+            current_coin_solution = solutions_array[val - coin] + [coin]
+          end
+        end
+      end
+      num_coins_needed[val] = current_coins_needed
+      solutions_array[val] = current_coin_solution
+    end
+
+    if solutions_array.last.length == 0
       raise ChangeError
     else
-      solution
+      solutions_array.last
     end
   end
 end
